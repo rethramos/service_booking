02c9fafe285622ed02e59140e218456090eeb9f6
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-18 18:48:52 +0800

    set up admin layout

---
+++ app/controllers/admin/admin_controller.rb
@@ -0,0 +1,17 @@
+class Admin::AdminController < ApplicationController
+  layout 'admin'
+
+  def ensure_logged_in
+    unless logged_in?
+      flash[:danger] = 'Please log in.'
+      redirect_to admin_login_url
+    end
+  end
+
+  # def ensure_admin
+  #   unless current_user.admin?
+  #     flash[:danger] = 'You can not access that page.'
+  #     redirect_to root_url
+  #   end
+  # end
+end
---
+++ app/controllers/admin/sessions_controller.rb
@@ -0,0 +1,21 @@
+class Admin::SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+
+    if @user && @user.authenticate(params[:session][:password]) && @user.admin?
+      log_in @user
+      redirect_to admin_root_url
+    else
+      flash.now[:danger] = 'Invalid credentials.'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to admin_login_url
+  end
+end
