144ea952848ef08b1cbe49195c009990d4023774
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-18 18:02:13 +0800

    add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, path: 'admin'
+  namespace :admin do
+    root 'static_pages#home'
+  end
+
+  devise_for :users, path: 'admin', controllers: {
+    sessions: 'admin/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
