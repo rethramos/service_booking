a959a9318f6699cb6e6b69f7b44097c4354d1c0c
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-18 18:50:52 +0800

    remove test for now

--- test/controllers/admin/static_pages_controller_test.rb
+++ test/controllers/admin/static_pages_controller_test.rb
@@ -1,8 +1,4 @@
 require "test_helper"
 
 class Admin::StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get admin_static_pages_home_url
-    assert_response :success
-  end
 end
