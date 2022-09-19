02c07f74d63d750d26435aba81e6e322e6312f59
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-19 18:10:46 +0800

    add validations

--- test/models/service_test.rb
+++ test/models/service_test.rb
@@ -1,7 +1,66 @@
 require "test_helper"
 
 class ServiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @service = businesses(:one).services.build(
+      name: 'Service 1',
+      description: 'Descriptions',
+      unit_price: 1500.00
+    )
+  end
+
+  test "should be valid" do
+    assert @service.valid?
+  end
+
+  test "should require name" do
+    @service.name = '    '
+    assert_not @service.valid?
+  end
+
+  test "name should be at most 100 characters" do
+    @service.name = 'a' * 101
+    assert_not @service.valid?
+  end
+
+  test "should require description" do
+    @service.description = '    '
+    assert_not @service.valid?
+  end
+
+  test "description should be at most 500 characters" do
+    @service.description = 'a' * 501
+    assert_not @service.valid?
+  end
+
+  test "should require unit price" do
+    @service.unit_price = '    '
+    assert_not @service.valid?
+  end
+
+  test "unit price should be numeric" do
+    @service.unit_price = '10O'
+    assert_not @service.valid?
+  end
+
+  test "unit price should be non-negative" do
+    @service.unit_price = -0.01
+    assert_not @service.valid?
+  end
+
+  test 'should require currency' do
+    @service.currency = '    '
+    assert_not @service.valid?
+  end
+
+  test 'currency should be at most 3 characters' do
+    @service.currency = 'USDS'
+    assert_not @service.valid?
+  end
+
+  test 'currency should be in uppercase' do
+    @service.currency = 'usd'
+    @service.save
+    assert @service.currency = 'USD'
+  end
 end
