5ebabb0279a0f922caeea5551371f8f891cb243d
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-19 12:54:47 +0800

    add error messages

--- app/controllers/admin/service_categories_controller.rb
+++ app/controllers/admin/service_categories_controller.rb
@@ -1,8 +1,15 @@
 class Admin::ServiceCategoriesController < Admin::AdminController
   def new
+    @service_category = ServiceCategory.new
   end
 
   def create
+    @service_category = ServiceCategory.new(service_category_params)
+    if @service_category.valid?
+
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
@@ -15,5 +22,11 @@ class Admin::ServiceCategoriesController < Admin::AdminController
   end
 
   def update
+  end
+
+  private
+
+  def service_category_params
+    params.require(:service_category).permit(:name, :description)
   end
 end
