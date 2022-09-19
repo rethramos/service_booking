0b31249ebb98932ca59dac2377bcac35c5799b9a
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-19 11:24:55 +0800

    add business validation

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,16 @@
 class Business < ApplicationRecord
   belongs_to :user
   has_one :address, as: :addressable
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :timezone, presence: true
+  validate :timezone_exists
+
+  private
+
+  def timezone_exists
+    return if timezone? && ActiveSupport::TimeZone[timezone].present?
+
+    errors.add(:timezone, 'is not supported')
+  end
 end
