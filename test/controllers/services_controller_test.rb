4cbcb7d550a4365f7123ce28dace1f44501fdb00
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-20 20:29:07 +0800

    update service controller tests

--- test/controllers/services_controller_test.rb
+++ test/controllers/services_controller_test.rb
@@ -3,46 +3,64 @@ require "test_helper"
 class ServicesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @service = services(:one)
+    @business = businesses(:one)
   end
 
   test "should get index" do
-    get services_url
-    assert_response :success
+    get admin_business_services_url(@service.business)
+    assert_redirected_to admin_login_url
   end
 
   test "should get new" do
-    get new_service_url
-    assert_response :success
+    get new_admin_business_service_url(@business)
+    assert_redirected_to admin_login_url
   end
 
   test "should create service" do
-    assert_difference("Service.count") do
-      post services_url, params: { service: { business_id: @service.business_id, description: @service.description, name: 'Another Service', service_category_id: @service.service_category_id, unit_price: @service.unit_price } }
+    assert_no_difference("Service.count") do
+      post admin_business_services_url(@business), params: {
+        service: {
+          business_id: @business.id,
+          description: @service.description,
+          name: 'Another Service',
+          service_category_id: @service.service_category_id,
+          unit_price: @service.unit_price
+        }
+      }
     end
 
-    assert_redirected_to service_url(Service.last)
+    assert_redirected_to admin_login_url
   end
 
   test "should show service" do
-    get service_url(@service)
-    assert_response :success
+    get admin_business_service_path(@service.business_id, @service)
+    assert_redirected_to admin_login_url
   end
 
   test "should get edit" do
-    get edit_service_url(@service)
-    assert_response :success
+    get edit_admin_business_service_url(@service.business, @service)
+    assert_redirected_to admin_login_url
   end
 
   test "should update service" do
-    patch service_url(@service), params: { service: { business_id: @service.business_id, description: @service.description, name: @service.name, service_category_id: @service.service_category_id, unit_price: @service.unit_price } }
-    assert_redirected_to service_url(@service)
+    patch admin_business_service_path(@service.business, @service), params: {
+      service: {
+        business_id: @service.business_id,
+        description: @service.description,
+        name: @service.name,
+        service_category_id:
+        @service.service_category_id,
+        unit_price: @service.unit_price
+        }
+    }
+    assert_redirected_to admin_login_url
   end
 
   test "should destroy service" do
-    assert_difference("Service.count", -1) do
-      delete service_url(@service)
+    assert_no_difference("Service.count") do
+      delete admin_business_service_path(@service.business, @service)
     end
 
-    assert_redirected_to services_url
+    assert_redirected_to admin_login_url
   end
 end
