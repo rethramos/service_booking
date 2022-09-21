cdef70eed59c9936661827de75577aff6ab2c267
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-21 15:07:22 +0800

    create service category type

--- app/graphql/types/service_category_type.rb
+++ app/graphql/types/service_category_type.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
 module Types
-  class ServiceType < Types::BaseObject
+  class ServiceCategoryType < Types::BaseObject
     field :id, ID, null: false
     field :business, Types::BusinessType, null: false
-    field :service_category, Types::ServiceCategoryType
     field :name, String, null: false
     field :description, String, null: false
-    field :unit_price, Float, null: false
-    field :currency, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
