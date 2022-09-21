a012489ed564d5cde85cc7ee588d1233052f4cd5
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-21 13:16:02 +0800

    add catch all page

--- app/javascript/router.js
+++ app/javascript/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from './views/HomePage.vue'
 import RegisterPage from './views/RegisterPage.vue'
 import LoginPage from './views/LoginPage.vue'
+import NotFound from './views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -9,6 +10,16 @@ const router = createRouter({
     { path: '/', name: 'home', component: HomePage },
     { path: '/register', name: 'register', component: RegisterPage },
     { path: '/login', name: 'login', component: LoginPage },
+    {
+      path: '/404',
+      name: '404',
+      component: NotFound,
+      props: true,
+    },
+    {
+      path: '/:catchAll(.*)',
+      redirect: { name: '404' },
+    },
     
   ],
 })
---
+++ app/javascript/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="">
+      <h1>404</h1>
+      <h2>Resource not found.</h2>
+      <RouterLink :to="{ name: 'home' }">Go back to home instead</RouterLink>
+    </div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style></style>
