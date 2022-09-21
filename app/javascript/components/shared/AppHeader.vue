98635b513d8076bcfa5374a3ab14f6fb95e9c883
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-21 16:22:39 +0800

    reload page on logout

--- app/javascript/components/shared/AppHeader.vue
+++ app/javascript/components/shared/AppHeader.vue
@@ -59,7 +59,7 @@ export default {
     logOut() {
       this.logoutUser().then((success) => {
         if (success) {
-          this.$router.push({ name: "home" });
+          window.location.reload()
         }
       });
     },
