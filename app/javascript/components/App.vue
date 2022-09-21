e80763a3081d1aa59fcd62bf6efe91e99d36d41a
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-21 13:00:41 +0800

    fetch logged in user on page load

--- app/javascript/components/App.vue
+++ app/javascript/components/App.vue
@@ -12,8 +12,15 @@
 import AppHeader from "./shared/AppHeader.vue";
 import AppFooter from "./shared/AppFooter.vue";
 import AppToast from "./shared/AppToast.vue";
+import { mapActions } from "vuex";
 export default {
   components: { AppHeader, AppFooter, AppToast },
+  methods: {
+    ...mapActions('auth', ['fetchCurrentUser'])
+  },
+  created() {
+    this.fetchCurrentUser()
+  }
 };
 </script>
 <style></style>
--- app/javascript/store/modules/auth.js
+++ app/javascript/store/modules/auth.js
@@ -1,5 +1,6 @@
 import apolloClient from "../../apollo";
 import { CREATE_USER } from "../../graphql/mutations";
+import { ME } from "../../graphql/queries";
 
 export const namespaced = true;
 
@@ -14,32 +15,51 @@ export const mutations = {
 };
 
 export const actions = {
+  async fetchCurrentUser({ commit }) {
+    const {
+      data: { me },
+    } = await apolloClient.query({
+      query: ME,
+    });
+    switch (me.__typename) {
+      case "User":
+        commit("SET_CURRENT_USER", me);
+        break;
+
+      default:
+        commit("SET_CURRENT_USER", null);
+        break;
+    }
+    return me;
+  },
   createUser(
     { commit },
     { firstName, lastName, timezone, email, password, passwordConfirmation }
   ) {
-    return apolloClient.mutate({
-      mutation: CREATE_USER,
-      variables: {
-        input: {
-          firstName,
-          lastName,
-          timezone,
-          authProvider: {
-            credentials: {
-              email,
-              password,
+    return apolloClient
+      .mutate({
+        mutation: CREATE_USER,
+        variables: {
+          input: {
+            firstName,
+            lastName,
+            timezone,
+            authProvider: {
+              credentials: {
+                email,
+                password,
+              },
+              passwordConfirmation,
             },
-            passwordConfirmation,
           },
         },
-      },
-    }).then(({ data: { createUser } }) => {
-      if (createUser.__typename === 'CreateUserPayload') {
-        commit('SET_CURRENT_USER', createUser.user)
-      }
-      return createUser
-    })
+      })
+      .then(({ data: { createUser } }) => {
+        if (createUser.__typename === "CreateUserPayload") {
+          commit("SET_CURRENT_USER", createUser.user);
+        }
+        return createUser;
+      });
   },
 };
 
