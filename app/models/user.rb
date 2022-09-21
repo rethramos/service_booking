b4a23dd87ad9bd8efde50a099e138ee57496d46b
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-21 13:04:53 +0800

    add timzeone validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
+  validates :timezone, presence: true
+  validate :timezone_exists
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -15,7 +17,6 @@ class User < ApplicationRecord
 
   validates :password, presence: true, length: { minimum: 6 }
 
-
   # Returns the hash digest of the given string.
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
@@ -23,6 +24,12 @@ class User < ApplicationRecord
   end
 
   private
+
+  def timezone_exists
+    return if timezone? && ActiveSupport::TimeZone[timezone].present?
+
+    errors.add(:timezone, 'is not supported')
+  end
 
   def downcase_email
     email.downcase!
