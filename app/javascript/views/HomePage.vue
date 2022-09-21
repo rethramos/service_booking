fea7ba8a28f1f9faff8b134637e98ac3e6aa98be
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-21 15:19:04 +0800

    create search bar

--- app/javascript/views/HomePage.vue
+++ app/javascript/views/HomePage.vue
@@ -1,14 +1,39 @@
 <template lang="">
   <div>
     <h1 class="text-center">Service Booking</h1>
-    TODO: add search bar, service category cards
+    <form @submit.prevent="searchServices">
+      <label for="service_search">Search services</label>
+      <div class="input-group">
+        <input
+          v-model="q"
+          type="search"
+          id="service_search"
+          name="q"
+          placeholder="Search haircuts"
+          class="form-control"
+        />
+        <BaseButton class="btn-primary">Search</BaseButton>
+      </div>
+    </form>
   </div>
 </template>
 <script>
+import BaseButton from "../components/shared/BaseButton.vue";
+
 export default {
-  
-}
+  components: {
+    BaseButton,
+  },
+  data() {
+    return {
+      q: "",
+    };
+  },
+  methods: {
+    searchServices() {
+      console.log(this.q);
+    },
+  },
+};
 </script>
-<style lang="">
-  
-</style>
+<style lang=""></style>
