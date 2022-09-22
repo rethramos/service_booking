f9d84a69be646fd1ad524683a8ea9fdafa8398cb
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-22 11:58:26 +0800

    implement placeBookings mutation

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,6 +3,7 @@
 module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
+    field :cart, Types::CartType, null: false
     field :first_name, String, null: false
     field :last_name, String, null: false
     field :email, String, null: false
