38177ababa1a2a7e5a8bbfb4e9933d82b1fc3b37
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-22 10:47:30 +0800

    implement createReceipt resolver

--- app/graphql/mutations/create_receipt.rb
+++ app/graphql/mutations/create_receipt.rb
@@ -1,6 +1,5 @@
 module Mutations
   class CreateReceipt < BaseMutation
-
     argument :payment_option_id, ID, required: true
     argument :first_name, String, required: true
     argument :last_name, String, required: true
@@ -8,9 +7,32 @@ module Mutations
     argument :contact_number, String, required: true
     argument :address, Types::Address::AddressInputType, required: true
 
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
+    type Types::Receipt::CreateReceiptResultType, null: false
+
+    def resolve(
+      payment_option_id:,
+      first_name:,
+      last_name:,
+      email:,
+      contact_number:,
+      address:
+    )
+
+      return :unauthenticated unless (user = context[:current_user])
+      receipt = user.receipts.build(
+        payment_option_id:,
+        first_name:,
+        last_name:,
+        email:,
+        contact_number:,   
+      )
+      receipt.build_address(**address)
+
+      if receipt.save
+        receipt
+      else
+        receipt.errors
+      end
+    end
   end
 end
--- app/graphql/types/address/address_input_type.rb
+++ app/graphql/types/address/address_input_type.rb
@@ -2,11 +2,11 @@
 
 module Types
   class Address::AddressInputType < Types::BaseInputObject
-    argument :line_one, String, null: false
-    argument :line_two, String, null: false
-    argument :city, String, null: false
-    argument :province, String, null: false
-    argument :country, String, null: false
-    argument :postal_code, String, null: false
+    argument :line_one, String, required: true
+    argument :line_two, String, required: true
+    argument :city, String, required: true
+    argument :province, String, required: true
+    argument :country, String, required: true
+    argument :postal_code, String, required: true
   end
 end
