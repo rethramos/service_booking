1606ab5ab72466dceef60fcbd991ea3e7f8c37dc
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-23 00:31:46 +0800

    update admin home with booking overview

--- app/controllers/admin/static_pages_controller.rb
+++ app/controllers/admin/static_pages_controller.rb
@@ -2,5 +2,16 @@ class Admin::StaticPagesController < Admin::AdminController
   def home
     @users = User.where admin: false
     @user_count = @users.count
+
+    @served_bookings = Booking.where(booking_status: BookingStatus.served).count
+    @cancelled_bookings = Booking.where(booking_status: BookingStatus.cancelled).count
+    @pending_bookings = Booking.where(booking_status: BookingStatus.pending).count
+
+    @most_booked_services = Booking
+                            .select(:service_id)
+                            .group(:service_id)
+                            .order('count_service_id desc')
+                            .count
+                            .map { |service_id, n| { service: Service.find(service_id), count: n } }
   end
 end
--- app/models/booking_status.rb
+++ app/models/booking_status.rb
@@ -6,4 +6,16 @@ class BookingStatus < ApplicationRecord
   has_many :bookings, dependent: :nullify
 
   validates :name, presence: true, length: { maximum: 100 }, uniqueness: true
+
+  def self.pending
+    BookingStatus.find_by(name: PENDING)
+  end
+
+  def self.cancelled
+    BookingStatus.find_by(name: CANCELLED)
+  end
+
+  def self.served
+    BookingStatus.find_by(name: SERVED)
+  end
 end
