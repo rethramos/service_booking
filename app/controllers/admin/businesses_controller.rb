8354667df88d9ee2bea083d70d6c97ac6995c96a
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-28 15:08:04 +0800

    implement business creation

--- app/controllers/admin/businesses_controller.rb
+++ app/controllers/admin/businesses_controller.rb
@@ -1,4 +1,18 @@
 class Admin::BusinessesController < Admin::AdminController
+  def new
+    @business = current_user.businesses.build
+    @business.build_address
+  end
+
+  def create
+    @business = current_user.businesses.build(business_params)
+    if @business.save
+      redirect_to admin_businesses_url
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def index
     @businesses = Business.all
   end
@@ -7,5 +21,15 @@ class Admin::BusinessesController < Admin::AdminController
     @business = Business.find params[:id]
     @service_categories = @business.service_categories
     @services = @business.services
+  end
+
+  private
+
+  def business_params
+    params.require(:business).permit(
+      :name,
+      :timezone,
+      address_attributes: %i[line_one line_two city province country postal_code]
+    )
   end
 end
