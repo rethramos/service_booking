ccf31613e9d03dcf650ec1f8e53194c5921fbd15
Rethaniel Ramos <rethanielramos@gmail.com>
2022-09-28 15:10:15 +0800

    write tests

--- test/controllers/admin/businesses_controller_test.rb
+++ test/controllers/admin/businesses_controller_test.rb
@@ -1,6 +1,16 @@
 require "test_helper"
 
 class Admin::BusinessesControllerTest < ActionDispatch::IntegrationTest
+  test "should redirect new to admin login for guests" do
+    get new_admin_business_path
+    assert_redirected_to admin_login_url
+  end
+
+  test "should redirect create to admin login for guests" do
+    post admin_businesses_path
+    assert_redirected_to admin_login_url
+  end
+
   test "should get index" do
     get admin_businesses_path
     assert_redirected_to admin_login_url
